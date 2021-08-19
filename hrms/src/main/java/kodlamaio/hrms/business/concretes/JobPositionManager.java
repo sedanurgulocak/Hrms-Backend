ae5f33e58de84b33c58b432dacc0275a47167285
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-08-19 17:17:30 +0300

    🔨Check if your job title exists, if not add it

--- hrms/src/main/java/kodlamaio/hrms/business/concretes/JobPositionManager.java
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/JobPositionManager.java
@@ -7,7 +7,10 @@ import org.springframework.stereotype.Service;
 
 import kodlamaio.hrms.business.abstracts.JobPositionService;
 import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.ErrorResult;
+import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.core.utilities.results.SuccessDataResult;
+import kodlamaio.hrms.core.utilities.results.SuccessResult;
 import kodlamaio.hrms.dataAccess.abstracts.JobPositionDao;
 import kodlamaio.hrms.entities.concretes.JobPosition;
 
@@ -25,6 +28,15 @@ public class JobPositionManager implements JobPositionService{
 	@Override
 	public DataResult<List<JobPosition>> getAll() {
 		return new SuccessDataResult<List<JobPosition>>(this.jobPositionDao.findAll(), "Data listelendi") ;
+	}
+
+	@Override
+	public Result add(JobPosition jobPosition) {
+		if(this.jobPositionDao.existsJobByTitle(jobPosition.getTitle())) {
+			return new ErrorResult("Bu iş başlığı zaten var");
+		}
+		this.jobPositionDao.save(jobPosition);
+		return new SuccessResult("İş başlığı eklendi");
 	}
 	
 	
