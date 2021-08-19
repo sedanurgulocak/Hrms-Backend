f9c4e6c9cdb07a0980c6d18f30b24368afa81b71
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-08-20 01:11:20 +0300

    🔨Add new methods existsCandidateByEmail and existsCandidateByNationalIdentity

--- hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/CandidateDao.java
+++ hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/CandidateDao.java
@@ -6,4 +6,6 @@ import kodlamaio.hrms.entities.concretes.Candidate;
 
 public interface CandidateDao extends JpaRepository<Candidate, Integer>{
 
+	boolean existsCandidateByEmail(String email);
+	boolean existsCandidateByNationalIdentity(String nationalIdentity);
 }
