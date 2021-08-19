35663dcb672d7bcac718c6b607097be48dbb5373
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-08-19 15:43:11 +0300

    🔨Update employer

--- hrms/src/main/java/kodlamaio/hrms/entities/concretes/Employer.java
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/Employer.java
@@ -1,5 +1,6 @@
 package kodlamaio.hrms.entities.concretes;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
@@ -18,10 +19,13 @@ import lombok.NoArgsConstructor;
 @PrimaryKeyJoinColumn(name="id")
 public class Employer extends User{
 
+	@Column(name="company_name")
 	private String companyName;
 	
+	@Column(name="web_address")
 	private String webAddress;
 	
+	@Column(name="phone_number")
 	private String phoneNumber;
 	
 	
