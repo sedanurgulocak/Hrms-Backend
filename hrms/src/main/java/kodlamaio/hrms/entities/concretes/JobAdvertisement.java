d0a9ff29c0cbc9d894c4c45983f0952e195ab50b
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-08-23 01:43:34 +0300

    ✨Add new entity jobAdvertisement

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/JobAdvertisement.java
@@ -0,0 +1,52 @@
+package kodlamaio.hrms.entities.concretes;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="job_advertisements")
+public class JobAdvertisement {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	
+	private int employerId;
+	
+	private int jobPositionId;
+	
+	private int companyId;
+	
+	private int cityId;
+	
+	@Column(name="description")
+	private String description;
+	
+	@Column(name="number_of_open_position")
+	private int numberOfOpenPosition;
+	
+	@Column(name="status")
+	private boolean status;
+	
+	@Column(name="create_date")
+	private Date createDate;
+	
+	@Column(name="end_date")
+	private Date endDate;
+	
+}
