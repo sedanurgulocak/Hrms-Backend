c32b098cab1f97a000ad3af3a25d6f2e458a3a76
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-08-23 01:55:03 +0300

    🔨Build ManyToOne relation with the company

--- hrms/src/main/java/kodlamaio/hrms/entities/concretes/JobAdvertisement.java
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/JobAdvertisement.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -48,5 +50,9 @@ public class JobAdvertisement {
 	
 	@Column(name="end_date")
 	private Date endDate;
+	
+	@ManyToOne()
+	@JoinColumn(name="company_id")
+	private Company company;
 	
 }
