b7b4b3f43369be161ab8860ea3ab1fdb085817f6
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-08-23 01:46:13 +0300

    🔨Add JsonIgnoreProperties annotation to company

--- hrms/src/main/java/kodlamaio/hrms/entities/concretes/Company.java
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/Company.java
@@ -7,6 +7,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +18,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name="companies")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler", "job_advertisements"})
 public class Company {
 	
 	@Id
