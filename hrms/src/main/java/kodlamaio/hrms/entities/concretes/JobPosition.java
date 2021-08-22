50e4d916f93070f01f5c59d27322cd12a4f9f800
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-08-23 01:46:29 +0300

    🔨Add JsonIgnoreProperties annotation to jobPosition

--- hrms/src/main/java/kodlamaio/hrms/entities/concretes/JobPosition.java
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/JobPosition.java
@@ -7,11 +7,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Data;
 
 @Entity
 @Data
 @Table(name="job_positions")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler", "job_advertisements"})
 public class JobPosition {
 	
 	@Id
