e38351f5a93564569a80d34a593c450a98ea8495
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-08-23 01:54:33 +0300

    🔨Build OneToMany relation with the job advertisement

--- hrms/src/main/java/kodlamaio/hrms/entities/concretes/Company.java
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/Company.java
@@ -1,10 +1,13 @@
 package kodlamaio.hrms.entities.concretes;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -29,4 +32,7 @@ public class Company {
 	@Column(name="name")
 	private String name;
 
+	@OneToMany(mappedBy="company")
+	private List<JobAdvertisement> jobAdvertisement;
+	
 }
