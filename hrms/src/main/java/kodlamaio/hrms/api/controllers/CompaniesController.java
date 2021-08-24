753bc1c562308b28239a506d6abc6d88fbf13e5c
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-08-25 00:52:28 +0300

    🔨Add RequestBody annotation

--- hrms/src/main/java/kodlamaio/hrms/api/controllers/CompaniesController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/CompaniesController.java
@@ -2,6 +2,7 @@ package kodlamaio.hrms.api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +23,7 @@ public class CompaniesController {
 	}
 	
 	@PostMapping("/add")
-	public Result add(Company company) {
+	public Result add(@RequestBody Company company) {
 		return this.companyService.add(company);
 	}
 
