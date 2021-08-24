e4546ab9190f63e8fae665923fbd9e6cd09efaeb
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-08-25 02:00:55 +0300

    🔨Add new method getAll

--- hrms/src/main/java/kodlamaio/hrms/business/concretes/CityManager.java
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/CityManager.java
@@ -1,11 +1,15 @@
 package kodlamaio.hrms.business.concretes;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kodlamaio.hrms.business.abstracts.CityService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.ErrorResult;
 import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.core.utilities.results.SuccessDataResult;
 import kodlamaio.hrms.core.utilities.results.SuccessResult;
 import kodlamaio.hrms.dataAccess.abstracts.CityDao;
 import kodlamaio.hrms.entities.concretes.City;
@@ -28,6 +32,11 @@ public class CityManager implements CityService{
 		}
 		this.cityDao.save(city);
 		return new SuccessResult("Şehir kaydedildi");
+	}
+
+	@Override
+	public DataResult<List<City>> getAll() {
+		return new SuccessDataResult<List<City>>(this.cityDao.findAll(), "Data listelendi");
 	}
 
 }
