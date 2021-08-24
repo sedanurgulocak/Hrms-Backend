56d9a9f77015cfed5799d82cad16ec13c4f15d40
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-08-25 00:19:31 +0300

    ✨Create company manager and implements service

---
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/CompanyManager.java
@@ -0,0 +1,33 @@
+package kodlamaio.hrms.business.concretes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kodlamaio.hrms.business.abstracts.CompanyService;
+import kodlamaio.hrms.core.utilities.results.ErrorResult;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.core.utilities.results.SuccessResult;
+import kodlamaio.hrms.dataAccess.abstracts.CompanyDao;
+import kodlamaio.hrms.entities.concretes.Company;
+
+@Service
+public class CompanyManager implements CompanyService{
+
+	private CompanyDao companyDao;
+	
+	@Autowired
+	public CompanyManager(CompanyDao companyDao) {
+		super();
+		this.companyDao = companyDao;
+	}
+
+	@Override
+	public Result add(Company company) {
+		if(this.companyDao.existsCompanyByName(company.getName())) {
+			return new ErrorResult("Bu şirket zaten kayıtlı");
+		}
+		this.companyDao.save(company);
+		return new SuccessResult("Şirket kaydedildi");
+	}
+
+}
