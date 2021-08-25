f4fb7b91d7c5adf8bff2804ce0873370f8d05cd1
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-08-25 23:59:29 +0300

    🔨Add getAllActiveAndByCompany method

--- hrms/src/main/java/kodlamaio/hrms/business/abstracts/JobAdvertisementService.java
+++ hrms/src/main/java/kodlamaio/hrms/business/abstracts/JobAdvertisementService.java
@@ -15,4 +15,6 @@ public interface JobAdvertisementService {
 	DataResult<List<JobAdvertisement>> getAllActives();
 	
 	DataResult<List<JobAdvertisement>> getAllSortedDate();
+	
+	DataResult<List<JobAdvertisement>> getAllActiveAndByCompany(String companyName);
 }
