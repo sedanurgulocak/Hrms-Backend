d21d209c14a8239626323565647c9f88ef035f70
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-08-25 22:07:33 +0300

    🔨Implement getAllSortedDate and sort descending by date

--- hrms/src/main/java/kodlamaio/hrms/business/concretes/JobAdvertisementManager.java
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/JobAdvertisementManager.java
@@ -3,6 +3,7 @@ package kodlamaio.hrms.business.concretes;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import kodlamaio.hrms.business.abstracts.JobAdvertisementService;
@@ -38,6 +39,16 @@ public class JobAdvertisementManager implements JobAdvertisementService{
 	@Override
 	public DataResult<List<JobAdvertisement>> getAllActives() {
 		return new SuccessDataResult<List<JobAdvertisement>>(this.jobAdvertisementDao.queryJobAdvertisementByStatusTrue(), "Aktif iş ilanları listelendi");
+	}
+
+	@Override
+	public DataResult<List<JobAdvertisement>> getAllSortedDate() {
+		//Sort sort = Sort.by(Sort.Direction.ASC, "createDate");
+		//return new SuccessDataResult<List<JobAdvertisement>>(this.jobAdvertisementDao.findAll(sort));
+	
+		List<JobAdvertisement> jobAdvertisements = jobAdvertisementDao.findAll(Sort.by(Sort.Direction.DESC, "createDate"));
+		return new SuccessDataResult<List<JobAdvertisement>>(jobAdvertisements, "İş ilanları tarihe göre sıralandı");
+
 	}
 
 }
