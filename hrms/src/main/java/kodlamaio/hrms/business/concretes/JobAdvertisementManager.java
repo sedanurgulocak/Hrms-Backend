805467a2747d8bb8c1e9fc2906ad9590b73cee37
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-08-25 20:39:31 +0300

    🔨Implement getAll method

--- hrms/src/main/java/kodlamaio/hrms/business/concretes/JobAdvertisementManager.java
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/JobAdvertisementManager.java
@@ -1,10 +1,14 @@
 package kodlamaio.hrms.business.concretes;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kodlamaio.hrms.business.abstracts.JobAdvertisementService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.core.utilities.results.SuccessDataResult;
 import kodlamaio.hrms.core.utilities.results.SuccessResult;
 import kodlamaio.hrms.dataAccess.abstracts.JobAdvertisementDao;
 import kodlamaio.hrms.entities.concretes.JobAdvertisement;
@@ -24,6 +28,11 @@ public class JobAdvertisementManager implements JobAdvertisementService{
 	public Result add(JobAdvertisement jobAdvertisement) {
 		this.jobAdvertisementDao.save(jobAdvertisement);
 		return new SuccessResult("İlan kaydedildi");
+	}
+
+	@Override
+	public DataResult<List<JobAdvertisement>> getAll() {
+		return new SuccessDataResult<List<JobAdvertisement>>(this.jobAdvertisementDao.findAll(),"Data listelendi");
 	}
 
 }
