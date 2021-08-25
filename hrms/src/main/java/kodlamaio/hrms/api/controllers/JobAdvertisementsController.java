badff4bbd62b884b09c85b7bb5251b59cb455f04
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-08-26 00:29:00 +0300

    🔨Add gut mapping for set status

--- hrms/src/main/java/kodlamaio/hrms/api/controllers/JobAdvertisementsController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/JobAdvertisementsController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,6 +51,11 @@ public class JobAdvertisementsController {
 	@GetMapping("/getAllActiveAndByCompany")
 	public DataResult<List<JobAdvertisement>> getAllActiveAndByCompany(String companyName){
 		return this.jobAdvertisementService.getAllActiveAndByCompany(companyName);
+	}
+	
+	@PutMapping("/setStatus/{id}")
+	public Result setJobAdvertisementStatus(@PathVariable int id) {
+		return this.jobAdvertisementService.setJobAdvertisementStatus(id);
 	}
 
 }
