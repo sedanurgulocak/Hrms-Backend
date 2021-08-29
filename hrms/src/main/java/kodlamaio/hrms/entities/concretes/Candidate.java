a96a906f16d6151c95a93624335dc3622c12fa27
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-08-29 23:01:51 +0300

    🔨Establish OneToMany relationship between candidate and education

--- hrms/src/main/java/kodlamaio/hrms/entities/concretes/Candidate.java
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/Candidate.java
@@ -1,9 +1,14 @@
 package kodlamaio.hrms.entities.concretes;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,6 +22,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Table(name="candidates")
 @PrimaryKeyJoinColumn(name="user_id")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler", "educations"})
 public class Candidate extends User{
 
 	@Column(name="first_name")
@@ -30,4 +36,8 @@ public class Candidate extends User{
 	
 	@Column(name="date_of_birth")
 	private String dateOfBirth;
+	
+	@OneToMany(mappedBy="candidate")
+	private List<Education> educations;
+	
 }
