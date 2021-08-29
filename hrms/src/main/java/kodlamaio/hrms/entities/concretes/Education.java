fa07e0c26cf79adedc34df617d73c02d16f6517c
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-08-29 22:47:55 +0300

    ✨Create education entity

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/Education.java
@@ -0,0 +1,50 @@
+package kodlamaio.hrms.entities.concretes;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="educations")
+public class Education {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="schoolName")
+	private String schoolName;
+	
+	@Column(name="degree")
+	private String degree;
+	
+	@Column(name="field_of_study")
+	private String fieldOfStudy;
+	
+	@Column(name="start_date")
+	private Date startDate;
+	
+	@Column(name="end_date")
+	private Date endDate;
+	
+	@Column(name="is_graduated")
+	private boolean isGraduated;
+	
+	@Column(name="grade")
+	private double grade;
+	
+	
+}
