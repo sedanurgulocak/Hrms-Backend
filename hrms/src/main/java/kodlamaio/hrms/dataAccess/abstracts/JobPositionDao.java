0bdcd8a3068695467bd25691ba2ab6e0b07846bd
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-08-31 23:58:32 +0300

    🔨Update jobPosition related classes references

--- hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/JobPositionDao.java
+++ hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/JobPositionDao.java
@@ -2,7 +2,7 @@ package kodlamaio.hrms.dataAccess.abstracts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import kodlamaio.hrms.entities.concretes.JobPosition;
+import kodlamaio.hrms.entities.concretes.jobs.JobPosition;
 
 public interface JobPositionDao extends JpaRepository<JobPosition, Integer>{
 
