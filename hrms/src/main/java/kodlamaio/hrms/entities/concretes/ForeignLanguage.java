d959354090c4b73f6c20f6374b1e6fb847be7128
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-08-31 23:09:51 +0300

    ✨Create foreignLanguage entity

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/ForeignLanguage.java
@@ -0,0 +1,36 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="foreign_languages")
+public class ForeignLanguage {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="candidate_id")
+	private int candidateId;
+	
+	@Column(name="language")
+	private String language;
+	
+	@Column(name="level")
+	private double level;
+	
+	
+}
