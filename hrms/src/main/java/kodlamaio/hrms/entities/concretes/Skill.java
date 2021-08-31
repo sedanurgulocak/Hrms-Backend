5b7df09a18ba6b5ed61843c4d64da4582284564e
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-08-31 23:29:59 +0300

    🔨Establish ManyToOne relationship between skill and candidate

--- hrms/src/main/java/kodlamaio/hrms/entities/concretes/Skill.java
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/Skill.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -23,9 +25,10 @@ public class Skill {
 	@Column(name="id")
 	private int id;
 	
-	@Column(name="candidate_id")
-	private int candidateId;
-	
 	@Column(name="name")
 	private String name;
+	
+	@ManyToOne()
+	@JoinColumn(name="candidate_id")
+	private Candidate candidate;
 }
