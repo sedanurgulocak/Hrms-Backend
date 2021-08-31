5fead507d5b02044c64ebbd04d7f7f9ade13fb85
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-08-31 23:15:05 +0300

    ✨Create socialLink entity

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/SocialLink.java
@@ -0,0 +1,35 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="social_links")
+public class SocialLink {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="candidate_id")
+	private int candidateId;
+	
+	@Column(name="github_link")
+	private String githubLink;
+	
+	@Column(name="linkedin_link")
+	private String linledinLink;
+	
+}
