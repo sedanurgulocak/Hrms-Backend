008acba6559e38751969f6a9c37e7225506d8132
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-01 23:45:37 +0300

    🔨Remove cloudinary configuration

--- hrms/src/main/java/kodlamaio/hrms/HrmsApplication.java
+++ hrms/src/main/java/kodlamaio/hrms/HrmsApplication.java
@@ -1,8 +1,8 @@
 package kodlamaio.hrms;
 
-import java.io.File;
+//import java.io.File;
 import java.io.IOException;
-import java.util.Map;
+//import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,8 +12,8 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import com.cloudinary.*;
-import com.cloudinary.utils.ObjectUtils;
+//import com.cloudinary.*;
+//import com.cloudinary.utils.ObjectUtils;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -22,13 +22,13 @@ public class HrmsApplication {
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(HrmsApplication.class, args);
 		
-		Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
-				"cloud_name", "seda",
-				"api_key", "612252143868788",
-				"api_secret", "6R4XYnlStKbbSIGz6GQq8DRWkqs",
-				"secure", true));
-		File file = new File("my_image.jpg");
-		Map uploadResult = cloudinary.uploader().upload(file, ObjectUtils.emptyMap());
+		/*
+		 * Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap( "cloud_name",
+		 * "seda", "api_key", "612252143868788", "api_secret",
+		 * "6R4XYnlStKbbSIGz6GQq8DRWkqs", "secure", true)); File file = new
+		 * File("my_image.jpg"); Map uploadResult = cloudinary.uploader().upload(file,
+		 * ObjectUtils.emptyMap());
+		 */
 	}
 	
 	@Bean
