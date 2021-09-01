e794ec2c43774b9263baff5db79881aeb1fc4806
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-01 23:30:30 +0300

    ✨Create cloudinary adapter

---
+++ hrms/src/main/java/kodlamaio/hrms/core/adapters/CloudinaryAdapter.java
@@ -0,0 +1,32 @@
+package kodlamaio.hrms.core.adapters;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+
+public class CloudinaryAdapter implements PhotoUploadService{
+
+	@Override
+	public void uploadToPhoto(String photoUrl) {
+		try {
+			FileReader fileReader = new FileReader("/hrms/src/main/resources/cloudinarySettings.properties");
+			Properties properties = new Properties();
+			properties.load(fileReader);
+			
+			Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
+					"cloud_name", properties.getProperty("cloud_name"),
+					"api_key", properties.getProperty("api_key"),
+					"api_secret", properties.getProperty("api_secret")
+					));
+			cloudinary.uploader().upload(photoUrl, ObjectUtils.asMap());
+		}catch(IOException e) {
+			e.printStackTrace();
+			System.out.println("Fotoğraf yüklenemedi");
+		}
+		
+	}
+
+}
