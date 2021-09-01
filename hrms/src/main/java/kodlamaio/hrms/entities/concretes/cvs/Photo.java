86e57dd9092eded65e9935e15200cf69f1c0dea5
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-01 23:52:09 +0300

    ✨Create photo entity

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/cvs/Photo.java
@@ -0,0 +1,31 @@
+package kodlamaio.hrms.entities.concretes.cvs;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="photos")
+public class Photo {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	private int candidateId;
+	
+	@Column(name="photo_url")
+	private String photoUrl;
+
+}
