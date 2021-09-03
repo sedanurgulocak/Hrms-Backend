76dccdd31522b15ed72b574588a1b60ec7d23942
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-03 20:34:33 +0300

    🔨Add getListEducationByCandidateId method

--- hrms/src/main/java/kodlamaio/hrms/business/abstracts/EducationService.java
+++ hrms/src/main/java/kodlamaio/hrms/business/abstracts/EducationService.java
@@ -1,10 +1,15 @@
 package kodlamaio.hrms.business.abstracts;
 
+import java.util.List;
+
+import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.entities.concretes.cvs.Education;
 
 public interface EducationService {
 	
 	Result add(Education education);
+	
+	DataResult<List<Education>> getListEducationByCandidateId(int candidateId);
 
 }
