20e11f2bdc77722486706fe127ff2085eb53b5bd
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-03 20:36:34 +0300

    🔨Implement getListEducationByCandidateId method

--- hrms/src/main/java/kodlamaio/hrms/business/concretes/EducationManager.java
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/EducationManager.java
@@ -1,10 +1,14 @@
 package kodlamaio.hrms.business.concretes;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kodlamaio.hrms.business.abstracts.EducationService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.core.utilities.results.SuccessDataResult;
 import kodlamaio.hrms.core.utilities.results.SuccessResult;
 import kodlamaio.hrms.dataAccess.abstracts.EducationDao;
 import kodlamaio.hrms.entities.concretes.cvs.Education;
@@ -20,11 +24,17 @@ public class EducationManager implements EducationService{
 		this.educationDao = educationDao;
 	}
 
-
 	@Override
 	public Result add(Education education) {
 		this.educationDao.save(education);
 		return new SuccessResult("Eğitim bilgisi eklendi");
+	}
+
+
+	@Override
+	public DataResult<List<Education>> getListEducationByCandidateId(int candidateId) {
+		return new SuccessDataResult<List<Education>>(this.educationDao.getByCandidateId(candidateId), "Adaya ait eğitim bilgileri listelendi");
+		
 	}
 
 }
