78644fe84628e0cd3f624735b2b80db31620fecd
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-03 20:33:59 +0300

    🔨Add getByCandidateId

--- hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/EducationDao.java
+++ hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/EducationDao.java
@@ -1,9 +1,12 @@
 package kodlamaio.hrms.dataAccess.abstracts;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import kodlamaio.hrms.entities.concretes.cvs.Education;
 
 public interface EducationDao extends JpaRepository<Education, Integer>{
 
+	List<Education> getByCandidateId(int candidateId);
 }
