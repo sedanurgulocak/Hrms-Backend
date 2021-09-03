a10c81e5567bb9a7d7911f90ff25116b530ec333
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-03 20:00:16 +0300

    ✨Create education manager

---
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/EducationManager.java
@@ -0,0 +1,30 @@
+package kodlamaio.hrms.business.concretes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kodlamaio.hrms.business.abstracts.EducationService;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.core.utilities.results.SuccessResult;
+import kodlamaio.hrms.dataAccess.abstracts.EducationDao;
+import kodlamaio.hrms.entities.concretes.cvs.Education;
+
+@Service
+public class EducationManager implements EducationService{
+
+	private EducationDao educationDao;
+	
+	@Autowired
+	public EducationManager(EducationDao educationDao) {
+		super();
+		this.educationDao = educationDao;
+	}
+
+
+	@Override
+	public Result add(Education education) {
+		this.educationDao.save(education);
+		return new SuccessResult("Eğitim bilgisi eklendi");
+	}
+
+}
