bc4cd3ab319a2994f76cf46954b3d80a4b3801da
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-05 23:45:33 +0300

    🔨Add sortEducation method and change the return type for getListEducationByCandidateId method

--- hrms/src/main/java/kodlamaio/hrms/business/concretes/EducationManager.java
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/EducationManager.java
@@ -1,5 +1,7 @@
 package kodlamaio.hrms.business.concretes;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,9 +34,18 @@ public class EducationManager implements EducationService{
 
 
 	@Override
-	public DataResult<List<Education>> getListEducationByCandidateId(int candidateId) {
-		return new SuccessDataResult<List<Education>>(this.educationDao.getByCandidateId(candidateId), "Adaya ait eğitim bilgileri listelendi");
+	public List<Education> getListEducationByCandidateId(int candidateId) {
+		List<Education> educationsByCandidate = new ArrayList<Education>();
+		educationsByCandidate = this.educationDao.getByCandidateId(candidateId);
+		return educationsByCandidate;
 		
+	}
+
+	@Override
+	public DataResult<List<Education>> sortEducation(int candidateId) {
+		
+		Collections.sort(this.getListEducationByCandidateId(candidateId), Collections.reverseOrder());
+		return new SuccessDataResult<List<Education>>(this.getListEducationByCandidateId(candidateId),"Adayın eğitim bilgileri sıralandı");
 	}
 
 }
