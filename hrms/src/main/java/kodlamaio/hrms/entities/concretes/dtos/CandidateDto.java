d6739f61b71c8c283cefc3c8654dddfe9a599824
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-08 00:00:25 +0300

    ✨Create Candidate data transfer object

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/dtos/CandidateDto.java
@@ -0,0 +1,21 @@
+package kodlamaio.hrms.entities.concretes.dtos;
+
+public class CandidateDto {
+	
+	public String firstName;
+	
+	public String lastName;
+	
+	public String email;
+	
+	public String password;
+	
+	public String passwordAgain;
+	
+	public String type;
+	
+	public String nationalIdentity;
+	
+	public String dateOfBirth;
+
+}
