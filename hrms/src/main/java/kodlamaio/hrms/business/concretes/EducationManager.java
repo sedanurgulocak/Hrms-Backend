9e6c368b8dc81a948c29a88d5ddadeb75821a5fb
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-10 22:34:42 +0300

    🔨Implement findAllByCandidateIdOrderByEndDateDesc method and update entity to educationDto

--- hrms/src/main/java/kodlamaio/hrms/business/concretes/EducationManager.java
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/EducationManager.java
@@ -1,13 +1,12 @@
 package kodlamaio.hrms.business.concretes;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kodlamaio.hrms.business.abstracts.EducationService;
+import kodlamaio.hrms.core.utilities.modelMapper.DtoConverterService;
 import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.core.utilities.results.SuccessDataResult;
@@ -26,27 +25,27 @@ public class EducationManager implements EducationService{
 		super();
 		this.educationDao = educationDao;
 	}
+	
+	@Autowired
+	private DtoConverterService dtoConverterService;
+
 
 	@Override
 	public Result add(EducationDto educationDto) {
-		this.educationDao.save(educationDto);
+		Education education = (Education) this.dtoConverterService.dtoToEntity(educationDto, Education.class);
+		this.educationDao.save(education);
 		return new SuccessResult("Eğitim bilgisi eklendi");
 	}
 
-
 	@Override
-	public List<Education> getListEducationByCandidateId(int candidateId) {
-		List<Education> educationsByCandidate = new ArrayList<Education>();
-		educationsByCandidate = this.educationDao.getByCandidateId(candidateId);
-		return educationsByCandidate;
-		
+	public DataResult<List<Education>> getListEducationByCandidateId(int candidateId) {
+		return new SuccessDataResult<List<Education>>(this.educationDao.getByCandidateId(candidateId));
 	}
 
+
 	@Override
-	public DataResult<List<Education>> sortEducation(int candidateId) {
-		
-		Collections.sort(this.getListEducationByCandidateId(candidateId), Collections.reverseOrder());
-		return new SuccessDataResult<List<Education>>(this.getListEducationByCandidateId(candidateId),"Adayın eğitim bilgileri sıralandı");
+	public DataResult<List<Education>> findAllByCandidateIdOrderByEndDateDesc(int candidateId) {
+		return new SuccessDataResult<List<Education>>(this.educationDao.findAllByCandidateIdOrderByEndDateDesc(candidateId), "Eğitimler mezuniyet yılına göre sıralandı");
 	}
 
 }
