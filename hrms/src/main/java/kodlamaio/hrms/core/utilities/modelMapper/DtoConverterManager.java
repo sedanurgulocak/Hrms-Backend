a229683e92f82a944526e3d76cd7c76bef5feee7
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-10 21:12:24 +0300

    🔨Add service annotation

--- hrms/src/main/java/kodlamaio/hrms/core/utilities/modelMapper/DtoConverterManager.java
+++ hrms/src/main/java/kodlamaio/hrms/core/utilities/modelMapper/DtoConverterManager.java
@@ -6,7 +6,9 @@ import java.util.stream.Collectors;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class DtoConverterManager  implements DtoConverterService{
 
 	private ModelMapper modelMapper;
