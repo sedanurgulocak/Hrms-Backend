df4519ec6e077f244f3c363368b7d986d1572c29
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-10 22:36:40 +0300

    🔥Remove temporal annotation import

--- hrms/src/main/java/kodlamaio/hrms/entities/concretes/jobs/JobAdvertisement.java
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/jobs/JobAdvertisement.java
@@ -10,8 +10,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 import kodlamaio.hrms.entities.concretes.users.Employer;
 import lombok.AllArgsConstructor;
