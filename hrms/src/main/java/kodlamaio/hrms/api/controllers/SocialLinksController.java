f544d18ba03dfa163dc7f4c780f45910ff047ff7
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-14 00:21:24 +0300

    🔨Add PutMapping

--- hrms/src/main/java/kodlamaio/hrms/api/controllers/SocialLinksController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/SocialLinksController.java
@@ -1,13 +1,16 @@
 package kodlamaio.hrms.api.controllers;
 
 import kodlamaio.hrms.business.abstracts.SocialLinkService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.entities.concretes.dtos.SocialLinkDto;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,5 +29,11 @@ public class SocialLinksController {
 	public Result add(@RequestBody SocialLinkDto socialLinkDto) {
 		
 		return this.socialLinkService.add(socialLinkDto);
+	}
+	
+	@PutMapping("/update")
+	public DataResult<SocialLinkDto> update(@RequestParam int candidateId, @RequestParam String githubLink, @RequestParam String linkedinLink){
+		
+		return this.socialLinkService.update(candidateId, githubLink, linkedinLink);
 	}
 }
