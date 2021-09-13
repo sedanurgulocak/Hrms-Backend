ecfd7cb8af8a953ba2bbe51f52ddb458bf1467e5
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-14 00:21:05 +0300

    🔨Implement update method

--- hrms/src/main/java/kodlamaio/hrms/business/concretes/SocialLinkManager.java
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/SocialLinkManager.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Service;
 
 import kodlamaio.hrms.business.abstracts.SocialLinkService;
 import kodlamaio.hrms.core.utilities.modelMapper.DtoConverterService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.core.utilities.results.SuccessDataResult;
 import kodlamaio.hrms.core.utilities.results.SuccessResult;
 import kodlamaio.hrms.dataAccess.abstracts.SocialLinkDao;
 import kodlamaio.hrms.entities.concretes.cvs.SocialLink;
@@ -31,6 +33,17 @@ public class SocialLinkManager implements SocialLinkService{
 		SocialLink socialLink = (SocialLink) this.dtoConverterService.dtoToEntity(socialLinkDto, SocialLink.class);
 		this.socialLinkDao.save(socialLink);
 		return new SuccessResult("Sosyal hesaplar eklendi");
+	}
+
+
+	@Override
+	public DataResult<SocialLinkDto> update(int candidateId, String githubLink, String linkedinLink) {
+		SocialLink socialLink = this.socialLinkDao.getByCandidateId(candidateId);
+		socialLink.setGithubLink(githubLink);
+		socialLink.setLinledinLink(linkedinLink);
+		this.socialLinkDao.save(socialLink);
+		SocialLinkDto socialLinkDto =(SocialLinkDto) this.dtoConverterService.dtoToEntity(socialLink, SocialLinkDto.class);
+		return new SuccessDataResult<SocialLinkDto>(socialLinkDto, "Sosyal hesaplar güncellendi");
 	}
 
 }
