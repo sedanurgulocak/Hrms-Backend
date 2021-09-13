bfa821eca72412b0b01733bcf1aed4a56b6f865d
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-13 23:01:59 +0300

    ✨Create SocialLinkDto

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/dtos/SocialLinkDto.java
@@ -0,0 +1,13 @@
+package kodlamaio.hrms.entities.concretes.dtos;
+
+import lombok.Data;
+
+@Data
+public class SocialLinkDto {
+
+	private int candidateId;
+	
+	private String githubLink;
+	
+	private String linledinLink;
+}
