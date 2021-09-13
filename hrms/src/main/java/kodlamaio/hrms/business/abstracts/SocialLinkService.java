e89e5003dab1ef0dc9b08b523e72da5adeed6716
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-14 00:20:49 +0300

    🔨Add update method

--- hrms/src/main/java/kodlamaio/hrms/business/abstracts/SocialLinkService.java
+++ hrms/src/main/java/kodlamaio/hrms/business/abstracts/SocialLinkService.java
@@ -1,10 +1,13 @@
 package kodlamaio.hrms.business.abstracts;
 
+import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.entities.concretes.dtos.SocialLinkDto;
 
 public interface SocialLinkService {
 	
 	Result add(SocialLinkDto socialLinkDto);
+	
+	DataResult<SocialLinkDto> update(int candidateId, String githubLink, String linkedinLink);
 
 }
