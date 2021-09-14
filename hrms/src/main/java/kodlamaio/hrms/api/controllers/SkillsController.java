575f66571b0e5e4ed5e179d94f7eabf4ac09376d
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-14 23:08:53 +0300

    ✨Create skills controller and add PostMapping

---
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/SkillsController.java
@@ -0,0 +1,31 @@
+package kodlamaio.hrms.api.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.hrms.business.abstracts.SkillService;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.entities.concretes.dtos.SkillDto;
+
+@RestController
+@RequestMapping("api/skills")
+public class SkillsController {
+	
+	private SkillService skillService;
+
+	@Autowired
+	public SkillsController(SkillService skillService) {
+		super();
+		this.skillService = skillService;
+	}
+	
+	@PostMapping("/add")
+	public Result add(@RequestBody SkillDto skillDto) {
+		
+		return this.skillService.add(skillDto);
+	}
+
+}
