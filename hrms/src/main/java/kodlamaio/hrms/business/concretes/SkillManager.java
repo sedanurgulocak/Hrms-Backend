f63a45c2248a87e07e73efa9b19bc8460317eca4
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-14 23:16:22 +0300

    🔨Imlement deleteById methos

--- hrms/src/main/java/kodlamaio/hrms/business/concretes/SkillManager.java
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/SkillManager.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Service;
 
 import kodlamaio.hrms.business.abstracts.SkillService;
 import kodlamaio.hrms.core.utilities.modelMapper.DtoConverterService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.core.utilities.results.SuccessDataResult;
 import kodlamaio.hrms.core.utilities.results.SuccessResult;
 import kodlamaio.hrms.dataAccess.abstracts.SkillDao;
 import kodlamaio.hrms.entities.concretes.cvs.Skill;
@@ -31,6 +33,13 @@ public class SkillManager implements SkillService{
 		this.skillDao.save(skill);
 		return new SuccessResult("Yetenek eklendi");
 		
+	}
+
+	@Override
+	public DataResult<SkillDto> deleteById(int id) {
+		Skill skill = this.skillDao.deleteById(id);
+		SkillDto skillDto = (SkillDto) this.dtoConverterService.dtoToEntity(skill, SkillDto.class);
+		return new SuccessDataResult<SkillDto>(skillDto, "Yetenek silindi");
 	}
 
 }
