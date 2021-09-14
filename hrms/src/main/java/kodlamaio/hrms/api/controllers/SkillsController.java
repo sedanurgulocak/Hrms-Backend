3680f71df68234b839fa9e3d7a096c47bafce64c
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-14 23:30:30 +0300

    🔨Add GetMapping for getSkillListByCandidateId

--- hrms/src/main/java/kodlamaio/hrms/api/controllers/SkillsController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/SkillsController.java
@@ -1,11 +1,15 @@
 package kodlamaio.hrms.api.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.hrms.business.abstracts.SkillService;
@@ -35,6 +39,12 @@ public class SkillsController {
 	public DataResult<SkillDto> deleteById(@PathVariable("id") int id){
 		
 		return this.skillService.deleteById(id);
+	}
+	
+	@GetMapping("/getSkillListByCandidateId")
+	public DataResult<List<SkillDto>> getSkillListByCandidateId(@RequestParam int candidateId){
+		
+		return this.skillService.getSkillListByCandidateId(candidateId);
 	}
 
 }
