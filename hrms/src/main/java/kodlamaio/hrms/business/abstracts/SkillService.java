9bdaaa8ec0724425b1d55633a11fda1b090b6af4
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-14 23:29:29 +0300

    🔨Add getSkillListByCandidateId method

--- hrms/src/main/java/kodlamaio/hrms/business/abstracts/SkillService.java
+++ hrms/src/main/java/kodlamaio/hrms/business/abstracts/SkillService.java
@@ -1,6 +1,8 @@
 package kodlamaio.hrms.business.abstracts;
 
 
+import java.util.List;
+
 import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.entities.concretes.dtos.SkillDto;
@@ -10,4 +12,6 @@ public interface SkillService {
 	Result add(SkillDto skillDto);
 
 	DataResult<SkillDto> deleteById(int id);
+	
+	DataResult<List<SkillDto>> getSkillListByCandidateId(int candidateId);
 }
