cbe9b7cde27ba3721fd3a16d776f2a4f11c80d4a
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-14 23:16:38 +0300

    🔨Add DeleteMapping

--- hrms/src/main/java/kodlamaio/hrms/api/controllers/SkillsController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/SkillsController.java
@@ -1,12 +1,15 @@
 package kodlamaio.hrms.api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.hrms.business.abstracts.SkillService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.entities.concretes.dtos.SkillDto;
 
@@ -26,6 +29,12 @@ public class SkillsController {
 	public Result add(@RequestBody SkillDto skillDto) {
 		
 		return this.skillService.add(skillDto);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public DataResult<SkillDto> deleteById(@PathVariable("id") int id){
+		
+		return this.skillService.deleteById(id);
 	}
 
 }
