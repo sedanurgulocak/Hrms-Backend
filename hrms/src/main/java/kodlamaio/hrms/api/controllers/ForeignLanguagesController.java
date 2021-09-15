ffb094091f1e51e247c3f43111218a0b8b0edcbb
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-15 23:12:54 +0300

    🔨Add DeleteMapping

--- hrms/src/main/java/kodlamaio/hrms/api/controllers/ForeignLanguagesController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/ForeignLanguagesController.java
@@ -1,12 +1,15 @@
 package kodlamaio.hrms.api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.hrms.business.abstracts.ForeignLanguageService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.entities.concretes.dtos.ForeignLanguageDto;
 
@@ -26,6 +29,12 @@ public class ForeignLanguagesController {
 	public Result add(@RequestBody ForeignLanguageDto foreignLanguageDto) {
 		
 		return this.foreignLanguageService.add(foreignLanguageDto);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public DataResult<ForeignLanguageDto> deleteById(@PathVariable("id") int id){
+		
+		return this.foreignLanguageService.deleteById(id);
 	}
 
 }
