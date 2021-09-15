6690676b9951973a3e00c6868c6ea8fa1f74e6f8
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-15 23:12:16 +0300

    🔨Add deleteById method

--- hrms/src/main/java/kodlamaio/hrms/business/abstracts/ForeignLanguageService.java
+++ hrms/src/main/java/kodlamaio/hrms/business/abstracts/ForeignLanguageService.java
@@ -1,5 +1,6 @@
 package kodlamaio.hrms.business.abstracts;
 
+import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.entities.concretes.dtos.ForeignLanguageDto;
 
@@ -7,4 +8,5 @@ public interface ForeignLanguageService {
 	
 	Result add(ForeignLanguageDto foreignLanguageDto);
 
+	DataResult<ForeignLanguageDto> deleteById(int id);
 }
