443ad05f05825629d166c4294b9adfe1c687f0c8
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-15 23:12:34 +0300

    🔨Implement deleteById method

--- hrms/src/main/java/kodlamaio/hrms/business/concretes/ForeignLanguageManager.java
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/ForeignLanguageManager.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Service;
 
 import kodlamaio.hrms.business.abstracts.ForeignLanguageService;
 import kodlamaio.hrms.core.utilities.modelMapper.DtoConverterService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.core.utilities.results.SuccessDataResult;
 import kodlamaio.hrms.core.utilities.results.SuccessResult;
 import kodlamaio.hrms.dataAccess.abstracts.ForeignLanguageDao;
 import kodlamaio.hrms.entities.concretes.cvs.ForeignLanguage;
@@ -30,6 +32,13 @@ public class ForeignLanguageManager implements ForeignLanguageService{
 		ForeignLanguage foreignLanguage = (ForeignLanguage) this.dtoConverterService.dtoToEntity(foreignLanguageDto, ForeignLanguage.class);
 		this.foreignLanguageDao.save(foreignLanguage);
 		return new SuccessResult("Yabancı dil eklendi");
+	}
+
+	@Override
+	public DataResult<ForeignLanguageDto> deleteById(int id) {
+		ForeignLanguage foreignLanguage = this.foreignLanguageDao.deleteById(id);
+		ForeignLanguageDto foreigLanguageDto = (ForeignLanguageDto) this.dtoConverterService.dtoToEntity(foreignLanguage, ForeignLanguageDto.class);
+		return new SuccessDataResult<ForeignLanguageDto>(foreigLanguageDto, "Yabancı dil silindi");
 	}
 
 }
