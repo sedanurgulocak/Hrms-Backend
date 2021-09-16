bc91ae91d348b80cc93b94db3a5579deaf02600a
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-16 22:47:00 +0300

    🔨Add getForeignLanguageListByCandidateId query

--- hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/ForeignLanguageDao.java
+++ hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/ForeignLanguageDao.java
@@ -1,5 +1,7 @@
 package kodlamaio.hrms.dataAccess.abstracts;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import kodlamaio.hrms.entities.concretes.cvs.ForeignLanguage;
@@ -7,5 +9,7 @@ import kodlamaio.hrms.entities.concretes.cvs.ForeignLanguage;
 public interface ForeignLanguageDao extends JpaRepository<ForeignLanguage, Integer>{
 
 	ForeignLanguage deleteById(int id);
+	
+	List<ForeignLanguage> getForeignLanguageListByCandidateId(int candidateId);
 	
 }
