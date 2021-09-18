63fab165e8e32df823d3048d476869e576ea4741
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-18 22:54:26 +0300

    🔨Implement getJobExperienceListByCandidateId method

--- hrms/src/main/java/kodlamaio/hrms/business/concretes/JobExperienceManager.java
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/JobExperienceManager.java
@@ -1,11 +1,15 @@
 package kodlamaio.hrms.business.concretes;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kodlamaio.hrms.business.abstracts.JobExperienceService;
 import kodlamaio.hrms.core.utilities.modelMapper.DtoConverterService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.core.utilities.results.SuccessDataResult;
 import kodlamaio.hrms.core.utilities.results.SuccessResult;
 import kodlamaio.hrms.dataAccess.abstracts.JobExperienceDao;
 import kodlamaio.hrms.entities.concretes.cvs.JobExperience;
@@ -30,6 +34,11 @@ public class JobExperienceManager implements JobExperienceService{
 		JobExperience jobExperience =(JobExperience) this.dtoConverterService.dtoToEntity(jobExperienceDto, JobExperience.class);
 		this.jobExperienceDao.save(jobExperience);
 		return new SuccessResult("Is tecrubesi eklendi");
+	}
+
+	@Override
+	public DataResult<List<JobExperienceDto>> getJobExperienceListByCandidateId(int candidateId) {
+		return new SuccessDataResult<List<JobExperienceDto>>(this.dtoConverterService.entityToDto(this.jobExperienceDao.getJobExperienceListByCandidateId(candidateId), JobExperienceDto.class), "İş tecrübeleri listelendi");
 	}
 
 }
