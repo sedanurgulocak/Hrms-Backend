251f63547346c4462cf5c884eb8608613617bda7
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-18 22:54:14 +0300

    🔨Add getJobExperienceListByCandidateId method

--- hrms/src/main/java/kodlamaio/hrms/business/abstracts/JobExperienceService.java
+++ hrms/src/main/java/kodlamaio/hrms/business/abstracts/JobExperienceService.java
@@ -1,10 +1,16 @@
 package kodlamaio.hrms.business.abstracts;
 
+import java.util.List;
+
+import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.entities.concretes.dtos.JobExperienceDto;
 
 public interface JobExperienceService {
 	
 	Result add(JobExperienceDto jobExperienceDto);
+	
+	DataResult<List<JobExperienceDto>> getJobExperienceListByCandidateId(int candidateId);
+	
 
 }
