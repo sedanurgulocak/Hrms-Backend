61aa2983e3107c4cd96a17a59d3d315c711d4118
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-18 22:45:44 +0300

    🔨Add getJobExperienceListByCandidateId query

--- hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/JobExperienceDao.java
+++ hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/JobExperienceDao.java
@@ -1,9 +1,12 @@
 package kodlamaio.hrms.dataAccess.abstracts;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import kodlamaio.hrms.entities.concretes.cvs.JobExperience;
 
 public interface JobExperienceDao extends JpaRepository<JobExperience, Integer>{
 
+	List<JobExperience> getJobExperienceListByCandidateId(int candidateId);
 }
