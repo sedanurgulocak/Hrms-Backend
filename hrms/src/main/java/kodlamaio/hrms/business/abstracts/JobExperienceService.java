7c5e7f483f39416f421d11ec64426fb0cbaba209
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-20 22:21:36 +0300

    🔨Update JobExperienceDto import

--- hrms/src/main/java/kodlamaio/hrms/business/abstracts/JobExperienceService.java
+++ hrms/src/main/java/kodlamaio/hrms/business/abstracts/JobExperienceService.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.Result;
-import kodlamaio.hrms.entities.concretes.dtos.JobExperienceDto;
+import kodlamaio.hrms.entities.concretes.dtos.cvDtos.JobExperienceDto;
 
 public interface JobExperienceService {
 	
