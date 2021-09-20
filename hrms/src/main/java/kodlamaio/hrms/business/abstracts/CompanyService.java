5d070d6c21725140aabbd84576b719cadcda9bcb
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-21 00:08:38 +0300

    🔨Change method parameter CompanyDto

--- hrms/src/main/java/kodlamaio/hrms/business/abstracts/CompanyService.java
+++ hrms/src/main/java/kodlamaio/hrms/business/abstracts/CompanyService.java
@@ -1,9 +1,9 @@
 package kodlamaio.hrms.business.abstracts;
 
 import kodlamaio.hrms.core.utilities.results.Result;
-import kodlamaio.hrms.entities.concretes.jobs.Company;
+import kodlamaio.hrms.entities.concretes.dtos.jobsDto.CompanyDto;
 
 public interface CompanyService {
 
-	Result add(Company company);
+	Result add(CompanyDto companyDto);
 }
