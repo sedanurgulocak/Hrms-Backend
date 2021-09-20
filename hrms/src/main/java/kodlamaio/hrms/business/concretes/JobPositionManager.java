d3914f6d85d263c0286fa5da928a68544f031fcf
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-21 00:18:22 +0300

    🔨Inject DtoConverterService and change methods parameters to JobPositionDto

--- hrms/src/main/java/kodlamaio/hrms/business/concretes/JobPositionManager.java
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/JobPositionManager.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kodlamaio.hrms.business.abstracts.JobPositionService;
+import kodlamaio.hrms.core.utilities.modelMapper.DtoConverterService;
 import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.ErrorResult;
 import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.core.utilities.results.SuccessDataResult;
 import kodlamaio.hrms.core.utilities.results.SuccessResult;
 import kodlamaio.hrms.dataAccess.abstracts.JobPositionDao;
+import kodlamaio.hrms.entities.concretes.dtos.jobsDto.JobPositionDto;
 import kodlamaio.hrms.entities.concretes.jobs.JobPosition;
 
 @Service
@@ -24,17 +26,22 @@ public class JobPositionManager implements JobPositionService{
 		super();
 		this.jobPositionDao = jobPositionDao;
 	}
+	
+	@Autowired
+	private DtoConverterService dtoConverterService;
 
 	@Override
-	public DataResult<List<JobPosition>> getAll() {
-		return new SuccessDataResult<List<JobPosition>>(this.jobPositionDao.findAll(), "Data listelendi") ;
+	public DataResult<List<JobPositionDto>> getAll() {
+		return new SuccessDataResult<List<JobPositionDto>>(this.dtoConverterService.entityToDto(this.jobPositionDao.findAll(), JobPositionDto.class), "Data listelendi") ;
 	}
 
 	@Override
-	public Result add(JobPosition jobPosition) {
-		if(this.jobPositionDao.existsJobByTitle(jobPosition.getTitle())) {
+	public Result add(JobPositionDto jobPositionDto) {
+		if(this.jobPositionDao.existsJobByTitle(jobPositionDto.getTitle())) {
 			return new ErrorResult("Bu iş başlığı zaten var");
 		}
+		
+		JobPosition jobPosition = (JobPosition) this.dtoConverterService.dtoToEntity(jobPositionDto, JobPosition.class);
 		this.jobPositionDao.save(jobPosition);
 		return new SuccessResult("İş başlığı eklendi");
 	}
