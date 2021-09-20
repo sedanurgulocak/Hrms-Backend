872ba2d8fa4d24ad21617f41e6f37a23d48262d3
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-09-20 23:31:07 +0300

    🔨Change methods parameters CityDto

--- hrms/src/main/java/kodlamaio/hrms/business/abstracts/CityService.java
+++ hrms/src/main/java/kodlamaio/hrms/business/abstracts/CityService.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.Result;
-import kodlamaio.hrms.entities.concretes.jobs.City;
+import kodlamaio.hrms.entities.concretes.dtos.jobsDto.CityDto;
 
 public interface CityService {
 
-	DataResult<List<City>> getAll();
+	DataResult<List<CityDto>> getAll();
 	
-	Result add(City city);
+	Result add(CityDto cityDto);
 }
